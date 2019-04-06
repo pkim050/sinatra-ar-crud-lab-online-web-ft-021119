7d7b41a89f10928bc65444c67557542668e602f2
Patrick Kim <pkim050@gmail.com>
2019-04-06 12:47:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
   post '/articles' do
     temp = Article.create(params)
     #binding.pry
-    redirect '/articles/:#{temp.id}'
+    redirect '/articles'
   end
 
   #Read
